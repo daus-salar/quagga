fb7c9815ee18980828c9e2fede5867602d8d60b7
Tobias Preuss <tobias.preuss@googlemail.com>
2013-11-03 13:43:01 +0100

    Implement device discovery.

--- QuaggaAndroid/Quagga/src/main/java/com/github/daussalar/quagga/MainActivity.java
+++ QuaggaAndroid/Quagga/src/main/java/com/github/daussalar/quagga/MainActivity.java
@@ -1,8 +1,12 @@
 package com.github.daussalar.quagga;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.MediaRouteButton;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +15,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.cast.CastContext;
+import com.google.cast.CastDevice;
+import com.google.cast.MediaRouteAdapter;
+import com.google.cast.MediaRouteHelper;
+import com.google.cast.MediaRouteStateChangeListener;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -50,7 +58,15 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements MediaRouteAdapter {
+
+        private CastContext mCastContext;
+        private MediaRouteButton mMediaRouteButton;
+        private MediaRouter mMediaRouter;
+        private MediaRouteSelector mMediaRouteSelector;
+        private MyMediaRouterCallback mMediaRouterCallback;
+        private CastDevice mSelectedDevice;
+        private MediaRouteStateChangeListener mRouteStateListener;
 
         public PlaceholderFragment() {
         }
@@ -62,15 +78,68 @@ public class MainActivity extends ActionBarActivity {
 
         @Override public void onViewCreated(View view, Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
-            Button sendBtn = (Button) getActivity().findViewById(R.id.btn_send);
+            Activity activity = getActivity();
+            Button sendBtn = (Button) activity.findViewById(R.id.btn_send);
             sendBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    CastContext castContext = new CastContext(getActivity());
+
                 }
             });
+
+            mMediaRouteButton = (MediaRouteButton) activity.findViewById(R.id.media_route_button);
+            mCastContext = new CastContext(activity.getApplicationContext());
+            MediaRouteHelper.registerMinimalMediaRouteProvider(mCastContext, this);
+            mMediaRouter = MediaRouter.getInstance(activity.getApplicationContext());
+            mMediaRouteSelector = MediaRouteHelper.buildMediaRouteSelector(
+                    MediaRouteHelper.CATEGORY_CAST);
+            mMediaRouteButton.setRouteSelector(mMediaRouteSelector);
+            mMediaRouterCallback = new MyMediaRouterCallback();
+
+            mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                    MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
         }
 
+        @Override public void onDestroyView() {
+            MediaRouteHelper.unregisterMediaRouteProvider(mCastContext);
+            mCastContext.dispose();
+            super.onDestroyView();
+        }
+
+        @Override public void onDetach() {
+            mMediaRouter.removeCallback(mMediaRouterCallback);
+            super.onDetach();
+        }
+
+        private class MyMediaRouterCallback extends MediaRouter.Callback {
+            @Override
+            public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
+                MediaRouteHelper.requestCastDeviceForRoute(route);
+            }
+
+            @Override
+            public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
+                mSelectedDevice = null;
+                mRouteStateListener = null;
+            }
+        }
+
+        // MediaRouteAdapter
+
+        @Override
+        public void onDeviceAvailable(CastDevice castDevice, String s,
+                                      MediaRouteStateChangeListener mediaRouteStateChangeListener) {
+            mSelectedDevice = castDevice;
+            mRouteStateListener = mediaRouteStateChangeListener;
+        }
+
+        @Override public void onSetVolume(double volume) {
+
+        }
+
+        @Override public void onUpdateVolume(double volume) {
+
+        }
     }
 
 }
